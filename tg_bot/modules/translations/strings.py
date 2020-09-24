6a777663c31fc1321b7f42ca946adbc0f3d8e7d5
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-09-24 14:25:03 +1000

    Update strings.py

--- tg_bot/modules/translations/strings.py
+++ tg_bot/modules/translations/strings.py
@@ -23,7 +23,21 @@ def tld(chat_id, t, show_none=True):
         else:
             return t
 
+def tld_list(chat_id, t):
+    LANGUAGE = prev_locale(chat_id)
 
+    if LANGUAGE:
+        LOCALE = LANGUAGE.locale_name
+        if LOCALE in ('en-US') and t in strings['en-US']:
+            return strings['en-US'][t]
+        elif LOCALE in ('en-GB') and t in strings['en-GB']:
+            return strings['en-GB'][t]
+        elif LOCALE in ('id') and t in strings['id']:
+            return strings['id'][t]
+        elif LOCALE in ('ru') and t in strings['ru']:
+            return strings['ru'][t]
+        elif LOCALE in ('es') and t in strings['es']:
+            return strings['es'][t]
 
 def tld_help(chat_id, t):
     LANGUAGE = prev_locale(chat_id)
