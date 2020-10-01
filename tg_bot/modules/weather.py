9d9be2fd93c7a32c120e79e2fd98dd7e93646cae
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-10-01 15:28:20 +1000

    Update weather.py

--- tg_bot/modules/weather.py
+++ tg_bot/modules/weather.py
@@ -54,10 +54,10 @@ def weather(bot, update, args):
         status += theweather._detailed_status
                         
 
-        update.message.reply_text("Today in {} is being {}, around {}°C.\n".format(thelocation,
+        update.message.reply_text("Today in {} it'll be {}, around {}°C.\n".format(thelocation,
                 status, temperature))
 
-    except pyowm.exceptions.not_found_error.NotFoundError:
+    except pyowm.exceptions.NotFoundError:
         update.effective_message.reply_text("Sorry, location not found.")
 
 
