3013ccae7d31492b4331fc8d0a3c9ae86e28e6d7
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-10-01 14:52:05 +1000

    Update feds.py

--- tg_bot/modules/feds.py
+++ tg_bot/modules/feds.py
@@ -81,7 +81,7 @@ def new_fed(bot: Bot, update: Update):
 		fed_name = fednam
 		LOGGER.info(fed_id)
 		if user.id == int(OWNER_ID):
-			fed_id = fed_name
+			fed_id = str(uuid.uuid4())
 
 		x = sql.new_fed(user.id, fed_name, fed_id)
 		if not x:
