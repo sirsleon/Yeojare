391af1e38d3c6b169eb41a0e83f248a3d670fd34
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-10-07 12:40:12 +1100

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,6 @@ from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK,
 from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
-from tg_bot.modules.translations.strings import tld, tld_help 
 from tg_bot.modules.connection import connected
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
