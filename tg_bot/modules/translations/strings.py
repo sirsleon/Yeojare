ee2f5e08fb8235330fc2f350652a2066c3fb0501
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-10-07 12:38:09 +1100

    Update strings.py

--- tg_bot/modules/translations/strings.py
+++ tg_bot/modules/translations/strings.py
@@ -1,27 +1,4 @@
 from tg_bot.modules.sql.locales_sql import prev_locale
-from tg_bot.modules.translations.English import EnglishStrings
-from tg_bot.modules.translations.Russian import RussianStrings
-from tg_bot.modules.translations.Ukraine import UkrainianStrings
-
-def tld(chat_id, t, show_none=True):
-    LANGUAGE = prev_locale(chat_id)
-    print(chat_id, t)
-    if LANGUAGE:
-        LOCALE = LANGUAGE.locale_name
-        if LOCALE in ('ru') and t in RussianStrings:
-           return RussianStrings[t]
-        elif LOCALE in ('uk') and t in UkrainianStrings:
-            return UkrainianStrings[t]
-        else:
-            if t in EnglishStrings:
-                return EnglishStrings[t]
-            else:
-                return t
-    elif show_none:
-        if t in EnglishStrings:
-            return EnglishStrings[t]
-        else:
-            return t
 
 def tld_list(chat_id, t):
     LANGUAGE = prev_locale(chat_id)
