2dd81ffe025acfcff2e96306775c4dd2ebc3d674
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-11-04 19:14:57 +1100

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -56,7 +56,7 @@ if ENV:
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
+    BAN_STICKER = os.environ.get('BAN_STICKER', False)
     KICK_STICKER = os.environ.get('KICK_STICKER', False)
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     API_WEATHER =os.environ.get('API_OPENWEATHER', False)
