0c2746e9073dd0f0717ef7238677400882bb210a
sirsleon <65666396+sirsleon@users.noreply.github.com>
2020-11-28 11:45:24 +1100

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -14,15 +14,12 @@ class Config(object):
     OWNER_ID = "594813047" # If you dont know, run @MissRose_bot and do /id in pm
     OWNER_USERNAME = "refundisillegal"
 
-    #Telethon [for purge stuff] Login to https://my.telegram.org and get these credentials
-    API_ID = None
-    API_HASH = None
-
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
     URL = None
 
     # OPTIONAL
@@ -31,12 +28,13 @@ class Config(object):
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
+    PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = True
+    STRICT_GBAN = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
     KICK_STICKER = False # StickerId while /kick ,same as BAN_STICKER
     ALLOW_EXCL = False  # Allow ! commands as well as /
-    CUSTOM_CMD = False  # Set to ('/', '!') or whatever to enable it, like ALLOW_EXCL but with more custom handler!
     API_OPENWEATHER = False #Get API_OPENWEATHER FROM OFFICAL SITE https://da.gd/VAW3
     TEMPORARY_DATA = None # Temporary data for backup module, use int number
     MAPS_API = ""
